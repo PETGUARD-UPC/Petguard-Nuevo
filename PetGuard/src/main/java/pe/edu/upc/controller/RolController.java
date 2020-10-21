91d62221a0ea12874558ff70b1d7ab678c9e5b53
AlexisCampos1 <71352065+AlexisCampos1@users.noreply.github.com>
2020-10-21 01:29:21 -0500

    Create RolController

---
+++ PetGuard/src/main/java/pe/edu/upc/controller/RolController.java
@@ -0,0 +1,51 @@
+package pe.edu.upc.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import pe.edu.upc.entity.Rol;
+import pe.edu.upc.serviceinterface.IRolService;
+
+@Controller
+@RequestMapping("/pets")
+public class RolController {
+
+	@Autowired
+	private IRolService rS;
+
+	@GetMapping("/new")
+	public String newRol(Model model) {
+		model.addAttribute("rol", new Rol());
+		return "rol/rol";
+	}
+
+	@PostMapping("/save")
+	public String saveRol(@Valid Rol rol, BindingResult result, Model model, SessionStatus status) throws Exception {
+		if (result.hasErrors()) {
+			return "rol/rol";
+		} else {
+			rS.insertar(rol);
+		}
+		model.addAttribute("listaRoles", rS.list());
+		return "/rol/listRol";
+	}
+
+	@GetMapping("/list")
+	public String listRols(Model model) {
+		try {
+			model.addAttribute("listRoles", rS.list());
+		} catch (Exception e) {
+			System.out.println("Error al listar en el controller");
+		}
+		return "/rol/listRol";
+	}
+
+}
