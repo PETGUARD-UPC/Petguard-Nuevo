a247c0ff12c5e1e5e9c611c49e784af1e6d0b631
KevinChav <u201614383@upc.edu.pe>
2020-10-20 22:30:23 -0500

    Create MascotaController

---
+++ PetGuard/src/main/java/pe/edu/upc/controller/MascotaController.java
@@ -0,0 +1,52 @@
+package pe.edu.upc.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import pe.edu.upc.entity.Mascota;
+import pe.edu.upc.serviceinterface.IMascotaService;
+
+@Controller
+@RequestMapping("/pets")
+public class MascotaController {
+	
+	@Autowired
+	private IMascotaService mS;
+	
+	@GetMapping("/new")
+	public String newMascota(Model model) {
+		
+		model.addAttribute("pet", new Mascota());
+		return "pet/pet";
+	}
+	
+	@PostMapping("/save")
+	public String saveMascota(@Valid Mascota pet,BindingResult result,Model model,
+			SessionStatus status) throws Exception{
+		if(result.hasErrors()) {
+			return"pet/pet";
+		}else {
+			mS.insert(pet);
+		}
+		model.addAttribute("listaMascotas",mS.list());
+		return "/pet/listPet";
+	}
+	
+	@GetMapping("/list")
+	public String listPets(Model model) {
+		try {
+			model.addAttribute("listMascotas",mS.list());
+		}catch(Exception e) {
+			System.out.println("Error al listar en el controller");
+		}
+		return "/pet/listPet";
+	}
+}
