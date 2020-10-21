f53ff0cf927117be5b798743eec8ce01f0b32697
sebastian <romero_villa-sebastian@hotmail.com>
2020-10-21 00:25:27 -0500

    Update Cliente

--- PetGuard/src/main/java/pe/edu/upc/entity/Cliente.java
+++ PetGuard/src/main/java/pe/edu/upc/entity/Cliente.java
@@ -25,7 +25,7 @@ public class Cliente {
 	
 	@ManyToOne
 	@JoinColumn(name = "idUser", nullable = false)
-	private int idUser;
+	private Usuario usuario;
 	
 	@Column(name = "name", length = 45, nullable = false)
 	private String name;
@@ -43,63 +43,80 @@ public class Cliente {
 	@Column(name = "birhtdate")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date Birthdate;
-	
+
 	public Cliente() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-	
-	public Cliente(int idCustomer, int idUser, String name, String lastname, int phone, String email, Date Birthdate) {
+
+	public Cliente(int idCustomer, Usuario usuario, String name, String lastname, int phone, String email,
+			Date birthdate) {
 		super();
 		this.idCustomer = idCustomer;
-		this.idUser = idUser;
+		this.usuario = usuario;
 		this.name = name;
 		this.lastname = lastname;
 		this.phone = phone;
 		this.email = email;
-		this.Birthdate = Birthdate;
+		Birthdate = birthdate;
 	}
-	
+
 	public int getIdCustomer() {
 		return idCustomer;
 	}
+
 	public void setIdCustomer(int idCustomer) {
 		this.idCustomer = idCustomer;
 	}
-	public int getIdUser() {
-		return idUser;
+
+	public Usuario getUsuario() {
+		return usuario;
 	}
-	public void setIdUser(int idUser) {
-		this.idUser = idUser;
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public int getPhone() {
 		return phone;
 	}
+
 	public void setPhone(int phone) {
 		this.phone = phone;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getBirthdate() {
 		return Birthdate;
 	}
+
 	public void setBirthdate(Date birthdate) {
 		Birthdate = birthdate;
 	}
+	
+
 	
 }
