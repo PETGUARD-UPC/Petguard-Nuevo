5da4b200282f9b73daa554b32afbf98ec9f1280e
sebastian <romero_villa-sebastian@hotmail.com>
2020-10-21 00:51:39 -0500

    Update Cuidador

--- PetGuard/src/main/java/pe/edu/upc/entity/Cuidador.java
+++ PetGuard/src/main/java/pe/edu/upc/entity/Cuidador.java
@@ -7,10 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "keeper")
+@Table(name = "keepers")
 public class Cuidador {
 
 	@Id
@@ -33,12 +35,18 @@ public class Cuidador {
 	private int salaryCuidador;
 	@Column(name = "experienceCuidador", nullable = false)
 	private int experienceCuidador;
+
+	@ManyToOne
+	@JoinColumn(name = "idUser", nullable = false)
+	private Usuario usuario;
+
 	public Cuidador() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Cuidador(int idCuidador, String nameCuidador, String surnameCuidador, int phoneCuidador, String mailCuidador,
-			Date birthCuidador, int scoreCuidador, int salaryCuidador, int experienceCuidador) {
+			Date birthCuidador, int scoreCuidador, int salaryCuidador, int experienceCuidador, Usuario usuario) {
 		super();
 		this.idCuidador = idCuidador;
 		this.nameCuidador = nameCuidador;
@@ -49,60 +57,89 @@ public class Cuidador {
 		this.scoreCuidador = scoreCuidador;
 		this.salaryCuidador = salaryCuidador;
 		this.experienceCuidador = experienceCuidador;
+		this.usuario = usuario;
 	}
+
 	public int getIdCuidador() {
 		return idCuidador;
 	}
+
 	public void setIdCuidador(int idCuidador) {
 		this.idCuidador = idCuidador;
 	}
+
 	public String getNameCuidador() {
 		return nameCuidador;
 	}
+
 	public void setNameCuidador(String nameCuidador) {
 		this.nameCuidador = nameCuidador;
 	}
+
 	public String getSurnameCuidador() {
 		return surnameCuidador;
 	}
+
 	public void setSurnameCuidador(String surnameCuidador) {
 		this.surnameCuidador = surnameCuidador;
 	}
+
 	public int getPhoneCuidador() {
 		return phoneCuidador;
 	}
+
 	public void setPhoneCuidador(int phoneCuidador) {
 		this.phoneCuidador = phoneCuidador;
 	}
+
 	public String getMailCuidador() {
 		return mailCuidador;
 	}
+
 	public void setMailCuidador(String mailCuidador) {
 		this.mailCuidador = mailCuidador;
 	}
+
 	public Date getBirthCuidador() {
 		return birthCuidador;
 	}
+
 	public void setBirthCuidador(Date birthCuidador) {
 		this.birthCuidador = birthCuidador;
 	}
+
 	public int getScoreCuidador() {
 		return scoreCuidador;
 	}
+
 	public void setScoreCuidador(int scoreCuidador) {
 		this.scoreCuidador = scoreCuidador;
 	}
+
 	public int getSalaryCuidador() {
 		return salaryCuidador;
 	}
+
 	public void setSalaryCuidador(int salaryCuidador) {
 		this.salaryCuidador = salaryCuidador;
 	}
+
 	public int getExperienceCuidador() {
 		return experienceCuidador;
 	}
+
 	public void setExperienceCuidador(int experienceCuidador) {
 		this.experienceCuidador = experienceCuidador;
 	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+
 
 }
