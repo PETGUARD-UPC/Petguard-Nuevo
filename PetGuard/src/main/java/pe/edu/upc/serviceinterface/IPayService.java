ffd7175c210eeeffe5436eea0dd55e612e1e0b34
Rafael150601 <71355987+Rafael150601@users.noreply.github.com>
2020-11-10 19:48:08 -0500

    Update pay
    Update Pay

--- PetGuard/src/main/java/pe/edu/upc/serviceinterface/IPayService.java
+++ PetGuard/src/main/java/pe/edu/upc/serviceinterface/IPayService.java
@@ -1,15 +1,19 @@
 package pe.edu.upc.serviceinterface;
 
 import java.util.List;
+import java.util.Optional;
 
 import pe.edu.upc.entity.Pay;
 
 public interface IPayService {
 
-	public void insert(Pay pay);
+	public int insert(Pay pay);
 	
 	public List<Pay>list();
 	
 	List<Pay>findBynamePay(String namePay);
 	
+	public void delete(int idPay);
+	
+	Optional<Pay> searchId(int idPay);
 }
